47645ca1bf91d3738d08f6e98125c548a6a1f0a0
victoriaw2408 <victoriaw2408@gmail.com>
2025-04-07 20:06:44 -0400

    added quiz folder

---
+++ src/Kambaz/Courses/Quizzes/Details.tsx
@@ -0,0 +1,15 @@
+import { Button } from "react-bootstrap";
+
+export default function QuizDetails() {
+    return (
+        <div>
+            <Button className="btn btn-lg btn-light btn-outline-secondary" id="wd-add-module-btn" >
+                Preview
+            </Button>
+
+            <Button className="btn btn-lg btn-light btn-outline-secondary" id="wd-add-module-btn" >
+                Edit
+            </Button>
+        </div>
+    );
+} 
---
+++ src/Kambaz/Courses/Quizzes/index.tsx
@@ -0,0 +1,8 @@
+import QuizDetails from "./Details";
+
+export default function Quizzes() {
+    return (
+        // <QuizDetails />
+        <hr />
+    );
+}
